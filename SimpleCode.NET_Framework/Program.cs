5207ce32530b879e68e901684821240b5dca1302
paulrwd85@gmail.com <paulrwd85@gmail.com>
2020-10-15 13:27:54 +0300

    Добавьте файлы проекта.

---
+++ SimpleCode.NET_Framework/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCode.NET_Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(myArray.Length);
+            Console.ReadLine();
+        }
+    }
+}
